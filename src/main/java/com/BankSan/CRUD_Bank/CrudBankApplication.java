67fbc2a3d492a2be60979db5e3a93962eca611b6
JesterinoInBox <toliksankin@gmail.com>
2026-02-07 13:32:49 +0300

    c

---
+++ src/main/java/com/BankSan/CRUD_Bank/CrudBankApplication.java
@@ -0,0 +1,13 @@
+package com.BankSan.CRUD_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBankApplication.class, args);
+	}
+
+}
