fff7e530cefc74751a84c76a1fb0c239d27cc62b
JesterinoInBox <toliksankin@gmail.com>
2026-02-07 22:03:28 +0300

    Полностью реализованы создание, удаление, просмотр и обновление над элементами.

--- src/main/java/com/BankSan/CRUD_Bank/CrudBankApplication.java
+++ src/main/java/com/BankSan/CRUD_Bank/CrudBankApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudBankApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(CrudBankApplication.class, args);
 	}
 
--- src/test/java/com/BankSan/CRUD_Bank/CrudBankApplicationTests.java
+++ src/test/java/com/BankSan/CRUD_Bank/CrudBankApplicationTests.java
@@ -2,8 +2,10 @@ package com.BankSan.CRUD_Bank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CrudBankApplicationTests {
 
 	@Test
