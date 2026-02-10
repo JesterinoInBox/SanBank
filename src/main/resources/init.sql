a8dae4ad94dcb063905a0fc90d6dc424fd9b5840
JesterinoInBox <toliksankin@gmail.com>
2026-02-10 15:48:25 +0300

    Была реализована система создания БД при её отсутсвии

---
+++ src/main/resources/init.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS clients (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    country_of_residence VARCHAR(32) NOT NULL,
+    city_of_residence VARCHAR(32) NOT NULL,
+    street_of_residence VARCHAR(32) NOT NULL,
+    year_of_birth SMALLINT NOT NULL
+);
