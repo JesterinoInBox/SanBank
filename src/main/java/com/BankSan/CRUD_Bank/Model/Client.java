b44c2c07fa07237c9b81ab087fdda9256d9f9d78
JesterinoInBox <toliksankin@gmail.com>
2026-02-10 16:25:51 +0300

    Реализован аудит изменения записей

--- src/main/java/com/BankSan/CRUD_Bank/Model/Client.java
+++ src/main/java/com/BankSan/CRUD_Bank/Model/Client.java
@@ -2,12 +2,14 @@ package com.BankSan.CRUD_Bank.Model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @Table(name = "clients")
-public class Client {
+public class Client extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
